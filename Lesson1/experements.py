b46ad8fba30a1e6ddff9d72ecc88efc1d27c04cb
redtomat87 <mr_tomat@mail.ru>
2021-05-31 22:47:01 +0300

    Homework1.1

---
+++ Lesson1/experements.py
@@ -0,0 +1,26 @@
+"""user_input = input("Введите число")  альтернативное решение task 3
+
+if not user_input.isdigit():
+    print("Неизвестный формат числа")
+    exit()
+
+result = 0
+
+for x in range(1, 4):
+    result += int(user_input * x)  # ‘3’ * 2 =33  так как user_input является строкой то допустим введённая 3 при умножении на 2 будет 33
+print(result)
+"""
+
+Второе решение
+user_number = int(input("Введите число"))  #альтернативное решение task 3
+
+if not user_input.isdigit():
+    print("Неизвестный формат числа")
+    exit()
+
+characters_cont = 0 #здесь будем считать количество цифр
+temp_number = user_number #создаем временную переменную для хранения того что ввел юзер
+
+while temp_number: # цикл пока существует число temp_number - здесь мы считаем кол-во символов в числе заданным пользователем деля его каждый раз на 10 и забирая целый остаток.
+    temp_number //= 10 # это более краткая версия записи temp_number = temp_number // 10
+    characters_cont += 1
--- Lesson1/task2.py
+++ Lesson1/task2.py
@@ -3,4 +3,4 @@ minute = user_time // 60
 hours = minute // 60
 ostatokMinut = user_time % 3600
 minute_ostatok = ostatokMinut // 60
-print(f"{user_time} секунд это {minute} минут или {hours} часов и {minute_ostatok} минут.")
+print(f"{user_time:>02} секунд это {minute:>02} минут или {hours:>02} часов и {minute_ostatok:>02} минут.")
